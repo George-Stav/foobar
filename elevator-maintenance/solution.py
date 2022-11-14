ceeede89938eb783d991aa767fb9ef166b5d3f36
George Stavropoulos <georgestavropoulos0@gmail.com>
2022-11-14 14:35:24 +0100

    elevator-maintenance solution

--- elevator-maintenance/solution.py
+++ elevator-maintenance/solution.py
@@ -1,61 +1,54 @@
 #!/usr/bin/env python2
 
-import timeit
-
 def solution(l):
-    return sorted(l)
+    # print(",".join(mergesort(l)))
+    print ",".join(mergesort(l))
 
-def mergeSort(a):
-    if len(a) > 1:
-        # r is the point where the array is divided into two subarrays
-        r = len(a) // 2 # be careful with this, python2 may behave differently
-        L = a[:r]
-        M = a[r:]
+def mergesort(arr):
+    if len(arr) > 1:
+        mid = len(arr) // 2
+        left = arr[:mid]
+        right = arr[mid:]
 
-        # Sort the two halves
-        mergeSort(L)
-        mergeSort(M)
+        mergesort(left)
+        mergesort(right)
 
         i = j = k = 0
 
-        # Until we reach either end of either L or M, pick larger among
-        # elements L and M and place them in the correct position at A[p..r]
-        while i < len(L) and j < len(M):
-            if compare(L[i], M[j]):
-                a[k] = L[i]
+        while i < len(left) and j < len(right):
+            if compare(left[i], right[j]):
+                arr[k] = left[i]
                 i += 1
             else:
-                a[k] = M[j]
+                arr[k] = right[j]
                 j += 1
             k += 1
 
-        # When we run out of elements in either L or M,
-        # pick up the remaining elements and put in A[p..r]
-        while i < len(L):
-            a[k] = L[i]
+        while i < len(left):
+            arr[k] = left[i]
             i += 1
             k += 1
 
-        while j < len(M):
-            a[k] = M[j]
+        while j < len(right):
+            arr[k] = right[j]
             j += 1
             k += 1
+
+    return arr
 
 def compare(a, b):
     al = a.split(".")
     bl = b.split(".")
-    print al, bl
 
-    if a[0] == b[0]:
-        # 2 vs 2.x
-        if len(al) == 1 or len(bl) == 1:
+    # compare by major, minor or revision numbers
+    for x in zip(al, bl):
+        # If major version number is the same (e.g. 2.0 vs 2.1)
+        # then continue to comparing the minor version number, and so on.
+        if x[0] != x[1]:
+            return int(x[0]) <= int(x[1])
 
-    else:
-        return a[0] < b[0]
+    # This case happens when one of the two version numbers is missing one or more integers
+    # e.g. 2 vs 2.0 OR 1.2.1 vs 1.2
+    return len(al) < len(bl)
 
-# print solution(["1.11", "2.0.0", "1.2", "2", "0.1", "1.2.1", "1.1.1", "2.0"])
-a = ["1.11", "2.0.0", "1.2", "2", "0.1", "1.2.1", "1.1.1", "2.0"]
-b = [5,2,4,6,6,1,2,5]
-print a
-mergeSort(a)
-print a
+solution(["1.11", "2.0.0", "1.2", "2", "0.1", "1.2.1", "1.1.1", "2.0"])
